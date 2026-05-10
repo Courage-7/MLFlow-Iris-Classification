17c86eeeb54c74cbf074f517d916d9d5f7db9e50
Courage Siameh <courage336@outlook.com>
2026-05-10 00:42:12 +0000

    build: setup project configuration and dependencies

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""Setup script for mlflow-iris."""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="mlflow-iris",
+    version="0.1.0",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    python_requires=">=3.10",
+)
