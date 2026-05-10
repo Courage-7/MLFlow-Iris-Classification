77eef26632edb3fce89b05d0d3bbbce4ba5a5918
Courage Siameh <courage336@outlook.com>
2026-05-10 01:08:47 +0000

    chore: update script shebangs to use uv run python

--- pipelines/predict.py
+++ pipelines/predict.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env -S uv run python
 """Run inference."""
 
 import argparse
--- pipelines/train_autolog.py
+++ pipelines/train_autolog.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env -S uv run python
 """Train with autologging."""
 
 import sys
